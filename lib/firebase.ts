4709659151cf5008e14293c91fc0541de728e319
Kaito <baseball2110k@gmail.com>
2026-09-05 17:22:49 +0900

    firebaseの設定を環境変数化

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -3,12 +3,12 @@ import { getAuth, GoogleAuthProvider } from "firebase/auth"
 import { getFirestore } from "firebase/firestore"
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAdl5B6YYG1Vgtxy46VOauwQX9kR9vpihg",
-  authDomain: "focus-app-b38bd.firebaseapp.com",
-  projectId: "focus-app-b38bd",
-  storageBucket: "focus-app-b38bd.firebasestorage.app",
-  messagingSenderId: "1024686822231",
-  appId: "1:1024686822231:web:bd2fe60935b97ee50f2412"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
 };
 
 const app = initializeApp(firebaseConfig)
